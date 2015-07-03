13c81dae5d4d25b49e5d1292dac5e7bf5e286fa0
Cindy <cindw@hotmail.com>
2015-07-03 13:14:52 -0400

    bean

--- jbeans.js
+++ jbeans.js
@@ -14,8 +14,12 @@ window.addEventListener('mousedown', function() {
     var test = document.createElement('img');
     console.log(width);
     test.setAttribute('src', 'test.png')
-    var width = test.clientWidth;
-    var height = test.clientHeight;
+    var width = 0;
+    var height = 0;
+    test.onload = function() {
+	width = this.width;
+	height = this.height;
+    }
     test.style.left = mouseX - width/2 + 'px';
     test.style.top = mouseY - height/2 + 'px'; 
     test.style.position = 'absolute';
