a12c1d66b21ccb10c5a478fc5f2b3a9ea4760a16
Cindy <cindw@hotmail.com>
2015-07-05 14:07:45 -0400

    beans

--- jbeans.js
+++ jbeans.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
     var mouseX = 0;
     var mouseY = 0;
+    document.getElementById('content').addEventListener('click', function() { 
+	var test = document.createElement('img');
+	test.setAttribute('src', 'test.png')
+	var width = 0;
+	var height = 0;
+	test.onload = function() {
+	    width = this.width;
+	    height = this.height;
+	}
+	test.style.left = mouseX - 150 + 'px';
+	test.style.top = mouseY - 150 + 'px'; 
+	test.style.position = 'absolute';
+	document.getElementById('body').appendChild(test); 
+	var audio = document.getElementById("testaudio");
+	audio.play();
+    });
 });
 
 window.addEventListener('mousemove', function(e) {
@@ -8,19 +24,3 @@ window.addEventListener('mousemove', function(e) {
     mouseY = e.pageY;
 });
 
-document.getElementById('content').addEventListener('click', function() { 
-    var test = document.createElement('img');
-    test.setAttribute('src', 'test.png')
-    var width = 0;
-    var height = 0;
-    test.onload = function() {
-	width = this.width;
-	height = this.height;
-    }
-    test.style.left = mouseX - 150 + 'px';
-    test.style.top = mouseY - 150 + 'px'; 
-    test.style.position = 'absolute';
-    document.getElementById('body').appendChild(test); 
-    var audio = document.getElementById("testaudio");
-    audio.play();
-});
