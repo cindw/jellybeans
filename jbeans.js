0254552bb5e4dd95f70a88ba12a5dde1bc0be9ce
Cindy <cindw@hotmail.com>
2015-07-06 13:29:54 -0400

    bean box

--- jbeans.js
+++ jbeans.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
     $('#content').bind('mousemove', function(e) {
 	mouseX = e.pageX;
 	mouseY = e.pageY;
-	console.log(mouseY);
     });
 
     $('#content').bind('click', function(e) {
