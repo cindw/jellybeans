92ede41e84fd22db2e2d1ddbf0baa173b20d0b0e
Cindy <cindw@hotmail.com>
2015-07-06 13:33:12 -0400

    bean box

--- jbeans.js
+++ jbeans.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var mouseX = 0;
     var mouseY = 0;
 
-    $('#content').bind('mousemove', function(e) {
+$('#content').bind('mousemove', function(e) {
 	mouseX = e.pageX;
 	mouseY = e.pageY;
     });
@@ -26,6 +26,32 @@ $(document).ready(function() {
 	images = document.getElementsByTagName('img');
 
     });
+    
+});
+
+$('#content').bind('mousemove', function(e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+});
+
+$('#content').bind('click', function(e) {
+
+    var test = document.createElement('img');
+    test.setAttribute('src', 'test.png')
+    var width = 0;
+    var height = 0;
+    test.onload = function() {
+	width = this.width;
+	height = this.height;
+    }
+    test.style.left = mouseX - 150 + 'px';
+    test.style.top = mouseY - 150 + 'px'; 
+    test.style.position = 'absolute';
+    document.getElementById('body').appendChild(test); 
+    var audio = document.getElementById("testaudio");
+    audio.play();
+    images = document.getElementsByTagName('img');
+
 });
 		  
 var images = [];
