e4fa64dcbbb77ddca8c2dfcbe00bdbdf22c9dbfc
Cindy <cindw@hotmail.com>
2015-07-06 13:13:25 -0400

    bean box

--- jbeans.js
+++ jbeans.js
@@ -1,38 +1,38 @@
 $(document).ready(function() {
     var mouseX = 0;
     var mouseY = 0;
-$('#content').bind('mousemove', function(e) {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-    console.log(mouseX);
-});
 
-$('#content').bind('click', function(e) {
-    var test = document.createElement('img');
-    test.setAttribute('src', 'test.png')
-    var width = 0;
-    var height = 0;
-    test.onload = function() {
-	width = this.width;
-	height = this.height;
-    }
-    test.style.left = mouseX - 150 + 'px';
-    test.style.top = mouseY - 150 + 'px'; 
-    test.style.position = 'absolute';
-    document.getElementById('body').appendChild(test); 
-    var audio = document.getElementById("testaudio");
-    audio.play();
-    images = document.getElementsByTagName('img');
-});
+    $('#content').bind('mousemove', function(e) {
+	mouseX = e.pageX;
+	mouseY = e.pageY;
+	console.log(mouseX);
+    });
+
+    $('#content').bind('click', function(e) {
+	var test = document.createElement('img');
+	test.setAttribute('src', 'test.png')
+	var width = 0;
+	var height = 0;
+	test.onload = function() {
+	    width = this.width;
+	    height = this.height;
+	}
+	test.style.left = mouseX - 150 + 'px';
+	test.style.top = mouseY - 150 + 'px'; 
+	test.style.position = 'absolute';
+	document.getElementById('body').appendChild(test); 
+	var audio = document.getElementById("testaudio");
+	audio.play();
+	images = document.getElementsByTagName('img');
+    });
 });
 
 var images = [];
 
-
 var gravity = function() {
 
     var wwidth = $(body).width();
-    var wheight = $(title).height() + $(content).height() + $(footer).height() + 30;
+    var wheight = $(title).height() + $(content).height() + 15;
     for ( var i = 0; i < images.length; i++ ) {
 	var pix = images[i].style.top;
 	var pixInt = parseInt( pix.substring(0, pix.length-2) );
