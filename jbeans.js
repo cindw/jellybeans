00908c6ab43a44e6b3c15131296d00fbaf7ddb2c
Cindy <cindw@hotmail.com>
2015-07-08 15:09:34 -0400

    testing canvas

--- jbeans.js
+++ jbeans.js
@@ -15,7 +15,7 @@ window.addEventListener('click', function(e) {
 	var imageObj = new Image();
 
 	imageObj.onload = function() {
-            context.drawImage(imageObj, 0, 0, 150, 50);
+            context.drawImage(imageObj, 0, 0, 0, 0);
 	};
 	imageObj.src = 'test.png';
 
