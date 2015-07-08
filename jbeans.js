9ae073d19ad6ee3e36736d59ed6591f86b4d281d
Cindy <cindw@hotmail.com>
2015-07-08 15:59:52 -0400

    testing canvas

--- jbeans.js
+++ jbeans.js
@@ -2,27 +2,20 @@ $(document).ready(function() {
     var mouseX = 0;
     var mouseY = 0;
 
-    function mousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-            x: evt.clientX - rect.left,
-            y: evt.clientY - rect.top
-        };
-    }
+    $("#screen").bind('mousemove', function(e) {
+	mouseX = e.pageX;
+	mouseY = e.pageY;
+    });
 
     $("#screen").bind('click', function(e) {
 	if ( mouseY > 100 ) {
-	    console.log("click");
 	    var canvas = document.getElementById('screen');
-	    var mouse = mousePos(canvas, e);
-
 	    var context = canvas.getContext('2d');
 	    var imageObj = new Image();
-
-	    console.log(mouse.x);
-	    console.log(mouse.y);
+	    console.log(mouseX);
+	    console.log(mouseY);
 	    imageObj.onload = function() {
-		context.drawImage(imageObj, mouse.x, mouse.y, 100, 50);
+		context.drawImage(imageObj, mouseX-500, mouseY-150, 100, 50);
 	    };
 	    imageObj.src = 'test.png';
 
