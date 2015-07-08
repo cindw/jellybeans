2b0cc8bc1aa5a5af997790f821a205c5dbb4dd3a
Cindy <cindw@hotmail.com>
2015-07-08 15:48:38 -0400

    testing canvas

--- jbeans.js
+++ jbeans.js
@@ -1,30 +1,32 @@
 $(document).ready(function() {
     var mouseX = 0;
     var mouseY = 0;
-});
 
-window.addEventListener('mousemove', function(e) {
+    $("#screen").bind('mousemove', function(e) {
 	mouseX = e.pageX;
 	mouseY = e.pageY;
     });
 
-window.addEventListener('click', function(e) {
-    if ( mouseY > 100 ) {
-	var canvas = document.getElementById('screen');
-	var context = canvas.getContext('2d');
-	var imageObj = new Image();
-	console.log(mouseX-600);
-	console.log(mouseY-300);
-	imageObj.onload = function() {
-            context.drawImage(imageObj, mouseX-600, mouseY-300, 100, 50);
-	};
-	imageObj.src = 'test.png';
+    $("#screen").bind('click', function(e) {
+	if ( mouseY > 100 ) {
+	    var canvas = document.getElementById('screen');
+	    var context = canvas.getContext('2d');
+	    var imageObj = new Image();
+	    console.log(mouseX-600);
+	    console.log(mouseY-300);
+	    imageObj.onload = function() {
+		context.drawImage(imageObj, mouseX-600, mouseY-300, 100, 50);
+	    };
+	    imageObj.src = 'test.png';
 
-	var audio = document.getElementById("testaudio");
-	audio.play();
-	images = document.getElementsByTagName('img');
-    }
+	    var audio = document.getElementById("testaudio");
+	    audio.play();
+	    images = document.getElementsByTagName('img');
+	}
+    });
 });
+
+
 
 var images = [];
 
